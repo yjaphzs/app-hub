a9f83a13a401f20163d4db33fb7e0f1bf4c9c78b
Jan Bautista <62127854+yjaphzs@users.noreply.github.com>
2026-04-09 23:17:58 +0800

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  base: "/",
-  build: {
-    outDir: "build",
-  },
-})
+});
